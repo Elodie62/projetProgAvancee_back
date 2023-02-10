b668c9871721de08779096d88e49ea24aed1f0d4
Melnoir4 <mlenoir4@myges.fr>
2023-02-10 11:55:59 +0100

    add bbd default value for product table

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,5 +3,7 @@ insert into catalogue (id, name) values (2, 'Sextoys Masculin');
 insert into catalogue (id, name) values (3, 'Accessoires');
 insert into catalogue (id, name) values (4, 'Lingeries');
 
-
-INSERT INTO `product`(`id`, `brand`, `catalogue_id`, `description`, `image`, `length`, `name`, `price`, `release_date`, `thickness`, `width`) VALUES (1,'Lelo',1,'Lelo est une marque de sextoys de luxe, fabriqués en Suède. Elle est connue pour ses produits haut de gamme, innovants et design.','TEST',20,'Lelo Hex',100,'2019-01-01',10,10);
+INSERT INTO `product`(`id`, `brand`, `catalogue_id`, `description`, `image`, `length`, `name`, `price`, `release_date`, `thickness`, `width`) VALUES (1,'Lelo',1,'Lelo est une marque de sextoys de luxe, fabriqués en Suède. Elle est connue pour ses produits haut de gamme, innovants et design.','TEST1',20,'Lelo Hex Feminin',100,'2019-01-01',10,10);
+INSERT INTO `product`(`id`, `brand`, `catalogue_id`, `description`, `image`, `length`, `name`, `price`, `release_date`, `thickness`, `width`) VALUES (2,'Lelo',2,'Lelo est une marque de sextoys de luxe, fabriqués en Suède. Elle est connue pour ses produits haut de gamme, innovants et design.','TEST2',20,'Lelo Hex Masculin',100,'2019-01-01',10,10);
+INSERT INTO `product`(`id`, `brand`, `catalogue_id`, `description`, `image`, `length`, `name`, `price`, `release_date`, `thickness`, `width`) VALUES (3,'Lelo',3,'Lelo est une marque de sextoys de luxe, fabriqués en Suède. Elle est connue pour ses produits haut de gamme, innovants et design.','TEST3',20,'Lelo Hex Accesoires',100,'2019-01-01',10,10);
+INSERT INTO `product`(`id`, `brand`, `catalogue_id`, `description`, `image`, `length`, `name`, `price`, `release_date`, `thickness`, `width`) VALUES (4,'Lelo',4,'Lelo est une marque de sextoys de luxe, fabriqués en Suède. Elle est connue pour ses produits haut de gamme, innovants et design.','TEST4',20,'Lelo Hex Linguerie',100,'2019-01-01',10,10);
